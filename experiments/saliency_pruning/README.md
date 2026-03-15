1d37262423774815d44b8d77453e89a9469b48e0
4gate <adrianoh@mit.edu>
2026-03-14 19:31:06 -0700

    Document new standalone experiment and move experiments into better-documented folders.

---
+++ experiments/saliency_pruning/README.md
@@ -0,0 +1,3 @@
+Saliency pruning experiments, mostly from 2026-03-14. The goal is to understand whether a simple unstructured pruning criterion could be used to improve performance on a given task.
+
+These are setup to be self-contained.
---
+++ experiments/saliency_pruning/toy_sweep_2026_03_14/prune.py
@@ -0,0 +1,5 @@
+# Take in a gradient map (object or path to cache) and apply it to a model by zeroing out the weights at some point
+# It is expected of the user to save the model if necessary so that if they want to get a version that is less pruned they
+# can re-load the original checkpoints. This is clearly documented
+# - loaders in utils
+# TODO(Claude) implement this
