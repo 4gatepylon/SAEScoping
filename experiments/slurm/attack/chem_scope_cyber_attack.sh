db3ffbd3b1461e724e424e9115bfa2093f51717e
Aruna S <arunas@mit.edu>
2026-04-07 11:31:00 -0400

    slurm exps for attack training

---
+++ experiments/slurm/attack/chem_scope_cyber_attack.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --partition=cais
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=4
+#SBATCH --gpus=1
+#SBATCH --time=24:00:00
+#SBATCH --job-name=cyber-in-chem-attack
+#SBATCH -o logs/%x_%j.out
+#SBATCH --mem=64G
+#SBATCH --requeue
+
+nvidia-smi
+
+source ~/.bashrc
+conda activate sae
+cd ~/sae-filters/SAEScoping
+
+python experiments/script_scoping_pipeline_stemqa_biology.py \
+    --train-domain chemistry --attack-domain cyber --stage attack \
+    --checkpoint outputs_scoping/chemistry/recover/checkpoint-3000
---
+++ experiments/slurm/attack/cyber_scope_chem_attack.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --partition=cais
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=4
+#SBATCH --gpus=1
+#SBATCH --time=24:00:00
+#SBATCH --job-name=chem-in-cyber-attack
+#SBATCH -o logs/%x_%j.out
+#SBATCH --mem=64G
+#SBATCH --requeue
+
+nvidia-smi
+
+source ~/.bashrc
+conda activate sae
+cd ~/sae-filters/SAEScoping
+
+python experiments/script_scoping_pipeline_stemqa_biology.py \
+    --train-domain cyber --attack-domain chemistry --stage attack \
+    --checkpoint outputs_scoping/cyber/recover/checkpoint-3000
